0281fc39f42c9144eb2c56d53ffed3ef196164af
Roy Siu <roysiu@outlook.com>
2017-04-09 19:44:00 +0000

    wrong version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open( path.join(path.abspath(path.dirname(__file__)) , 'README.md' ), encod
 
 setup(
     name = 'vernam',
-    version = '1.0.0',
+    version = '1.0.2',
 
     description = 'Vernam cipher',
     long_description = long_description,
