907feb3df591dd989b9f017aba544ea53cc8a746
Roy Siu <roysiu@outlook.com>
2017-04-09 09:11:06 +0000

    tidied up

--- vernam/vernam.py
+++ vernam/vernam.py
@@ -19,33 +19,12 @@ class vernam(object):
     #def vernam(self, key: Union[list, str]) -> list:
     def vernam(self, key):
         binkey = [ ord(x) for x in key ]
-        
-        """
-        app = 0
-        while len(binkey) < len(self.bintext):
-            binkey.append( binkey[app] )
-            app += 1
-        """
-        
+
         for i in range(len(self.bintext)):
             binkey.append( binkey[i] )
-        
-        """
-        result = []
-        for i in range(len(self.bintext)):
-            result.append( self.bintext[i] ^ binkey[i] )
-        """
+
         result = [ x for x in [ self.bintext[i] ^ binkey[i] for i in range(len(self.bintext)) ] ]
         
         for index, item in enumerate(result):
             result[index] = unichr(item)
         return result
-
-#"""
-foo = vernam("abcd6z")
-x = (foo.vernam("abczz"))
-print( x )
-bar = vernam(x)
-y = "".join(bar.vernam(["a","b","c","z","z"]))
-print( y )
-#"""
