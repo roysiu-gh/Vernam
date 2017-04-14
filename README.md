5ac42d783df7302fe6bfc4f1a94b47a8c83b4805
frimlinso <roysiu@outlook.com>
2017-04-14 00:52:34 +0000

    used absolute import in __main.py to ensure compatibility with python3 and pip3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ---
-# Vernam 2.5.5
+# Vernam 2.5.6
 
 ### Synopsis
 A simple vernam (or OTP) encryption / decription program in python
--- vernam/__main__.py
+++ vernam/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from py_vernam import vernam, usage
+from .py_vernam import vernam, usage
 import sys
 
 def main():
